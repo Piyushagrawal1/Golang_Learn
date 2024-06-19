a0f3403c489400c84c8fe0b047cc8cd8538cfbe0
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-06-19 11:16:55 +0530

    Variables in Golang

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
-	"MyLearning/myutil"
 	"fmt"
 )
 
 func main() {
 
 	fmt.Println("Hello, World!")
+	// myutil.PrintMessage("Hello, bhai kya hal hai!")
 
-	myutil.PrintMessage("Hello, bhai kya hal hai!")
+	//creating variables
+	// var name string = "Piyush"
+	// var version = 1.2
+	// fmt.Println(name)
+	// fmt.Println(version)
+
+	// var money int = 50000
+	// var currency = 1245
+	// fmt.Println(money)
+	// fmt.Println("Currenct: ", currency)
+
+	// var height float64 = 58.45
+	// fmt.Println(height)
+
+	// var isAdult bool = true
+	// // isAdult = false
+	// fmt.Println(isAdult)
+
+	// var person = "Piyush Agrawal"
+	// fmt.Println(person)
+
+	// //constant value
+	// const pi = 3.14
+	// fmt.Println(pi)
+
+	person := "piyush Agrawal"
+	fmt.Println(person)
+
 }
