ceb80b390421802162f690b07b2067f0b9db06c8
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-06-19 12:01:30 +0530

    taking user Input using bufio

--- userInput/main.go
+++ userInput/main.go
@@ -10,10 +10,13 @@ func main() {
 	fmt.Println("Hey, What's your name?")
 	//taking user input
 	//it print only before spaces
-	var name string
-	fmt.Scan(&name)
-	fmt.Println("Hello,", name)
+	// var name string
+	// fmt.Scan(&name)
+	// fmt.Println("Hello,", name)
 
-	
+	//using bufio - Print all
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n') // jb tk nyi line nahi mil jti tb tk print kr do
+	fmt.Println("Hello,", name)
 
 }
